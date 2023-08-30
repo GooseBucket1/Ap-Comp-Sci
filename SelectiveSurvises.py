14359977d4797b371541af387445d1081d8e1c30
GooseBucket1 <117537410+GooseBucket1@users.noreply.github.com>
2023-08-30 16:27:03 -0400

    8/30/2023
    This is something we did during class today nothing to big but the goal of it was to have only specific things be true like you had to be 18 or older and male

---
+++ SelectiveSurvises.py
@@ -0,0 +1,27 @@
+
+age = int(input("How old are you? "))
+gender = input("What is your gender? ")
+
+elegible_for_select_surv = age >= 18 and gender == "male"
+
+print ("is eligibal for selective survises " + str(elegible_for_select_surv))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
